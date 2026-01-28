96069a2b947241e1c05d5a33ea2d18873ce8a2a2
aarav <e1399165@u.nus.edu>
2026-01-29 00:33:04 +0800

    feature: Level-2 (Add, List)

--- src/main/java/Goofy.java
+++ src/main/java/Goofy.java
@@ -13,18 +13,28 @@ public class Goofy {
         System.out.print(" Hello! I'm \n" + logo + " What can I do for you?\n");
         System.out.println(line);
 
+        String[] tasks = new String[100];
+        int taskCounter = 0;
         Scanner in = new Scanner(System.in);
+
         while (true) {
             System.out.println();
             String input = in.nextLine();
-
             if (input.equalsIgnoreCase("bye")) {
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println(indent + line);
+                for (int i = 0; i < taskCounter; i++) {
+                    System.out.println(indent + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(indent + line);
+            } else {
+                tasks[taskCounter] = input;
+                taskCounter++;
+                System.out.println(indent + line);
+                System.out.println(indent + "added: " + input);
+                System.out.println(indent + line);
             }
-
-            System.out.println(indent + line);
-            System.out.println(indent + input);
-            System.out.println(indent + line);
         }
         System.out.println(indent + line);
         System.out.println(indent + "Bye. Hope to see you again soon!");
